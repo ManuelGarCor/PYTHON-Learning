d8c3451101404f77e0bb52e60bb7474f08ec7295
Manuel GC <manugaco@usal.es>
2024-05-08 09:33:07 +0200

    formatting

--- Teoria-Criptografía/calcular_producto.py
+++ Teoria-Criptografía/calcular_producto.py
@@ -9,5 +9,5 @@ primo1 = 95779082566233494946819232097249927580065143651692448119025606882506187
 primo2 = 9577908256623349494681923209724992758006514365169244811902560688250618757872932285936030165430188153818988990503064992274419390609277508426598746986029589
 
 producto, longitud_en_bits = calcular_producto_y_longitud(primo1, primo2)
-print(f"Producto de los primos (n=p*q):\n{producto}")
-print(f"Longitud en bits del producto:\n{longitud_en_bits}")
+print(f"Producto de los primos (n=p*q):\n{producto}\n")
+print(f"Longitud en bits del producto:\n{longitud_en_bits}\n")
